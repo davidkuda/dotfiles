b3ec58b42e42d34fdc403364aeaabf78dc33f175
David Kuda <david.kuda@noser.com>
2024-03-04 21:50:58 +0100

    add apt installs

---
+++ apt_install.sh
@@ -0,0 +1,16 @@
+sudo apt update
+
+sudo apt install \
+    diff-so-fancy \
+    figlet \
+    fzf \
+    git \
+    htop \
+    jq \
+    lsd \
+    miniforge \
+    ncdu \
+    neovim \
+    tmux \
+    tree \
+    zoxide

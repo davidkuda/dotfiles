5ec305f3cc28ff77bb494632b5d6c39f354b7232
David Kuda <david.kuda@noser.com>
2024-08-21 14:29:46 +0200

    update keymaps of harpoon

--- dot_config/nvim/lua/custom/plugins/harpoon.lua
+++ dot_config/nvim/lua/custom/plugins/harpoon.lua
@@ -7,13 +7,16 @@ return {
       ---@diagnostic disable-next-line: missing-parameter
       harpoon:setup()
 
-      vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<C-m>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-i>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>6", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>7", function() harpoon:list():select(4) end)
 
       -- Toggle previous & next buffers stored within Harpoon list
       -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
--- dot_config/nvim/lua/custom/plugins/oil.lua
+++ dot_config/nvim/lua/custom/plugins/oil.lua
@@ -1,13 +1,13 @@
 return {
   {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require("oil").setup {
-        columns = { "icon" },
+      require('oil').setup {
+        columns = { 'icon' },
         keymaps = {
-          ["<C-h>"] = false,
-          ["<M-h>"] = "actions.select_split",
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
         },
         view_options = {
           show_hidden = true,
@@ -15,11 +15,7 @@ return {
       }
 
       -- Open parent directory in current window
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-      vim.keymap.set("n", "<Leader>n", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
-      -- Open parent directory in floating window
-      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+      vim.keymap.set('n', '<Leader>n', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
   },
 }

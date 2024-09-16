aab5d27c8f3978fcf26d5fd2cf205a720e39a198
David Kuda <david.kuda@noser.com>
2024-09-16 22:57:08 +0200

    update k8s sh

--- dot_shelltools/kubernetes.sh
+++ dot_shelltools/kubernetes.sh
@@ -1,4 +1,12 @@
+autoload -Uz compinit
+compinit
+
 # https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
 source <(kubectl completion zsh)
 
 alias k=kubectl
+
+alias kx='kubectl exec'
+alias kxi='kubectl exec -ti'
+
+alias kpf='kubectl port-forward'

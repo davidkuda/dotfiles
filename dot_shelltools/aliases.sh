d71d6dd944c83091a2609292f807fcecb686df3f
David Kuda <david.kuda@noser.com>
2024-09-18 16:41:45 +0200

    update aliases

--- dot_shelltools/aliases.sh
+++ dot_shelltools/aliases.sh
@@ -2,6 +2,9 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
+alias -g L='| less -S'
+alias -g G='| grep -i'
+
 alias ls='lsd'
 alias la='ls -lAh'
 
@@ -12,7 +15,8 @@ alias ccp='xclip -selection clipboard'
 alias conda=$HOME/miniforge3/bin/conda
 
 # confs
+alias chere='chezmoi apply && exec zsh'
+alias ccd='chezmoi cd'
 alias confz='nvim $HOME/.local/share/chezmoi/'
 alias conft='nvim $HOME/.local/share/chezmoi/dot_tmux.conf'
 alias confn='nvim $HOME/.local/share/chezmoi/dot_config/nvim/'
-alias chere='chezmoi apply && exec zsh'

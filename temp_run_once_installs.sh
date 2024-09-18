3e4c1522771761135761209cc2eb019e239d7c66
David Kuda <david.kuda@noser.com>
2024-09-18 14:19:32 +0200

    update run once

--- temp_run_once_installs.sh
+++ temp_run_once_installs.sh
@@ -1,5 +1,4 @@
 # TODO:
-# - histdb
 # - starship
 # - neovim
 # - lsd
@@ -7,6 +6,8 @@
 
 
 # TODO: Check whether it's a debian OS before executing apt ...
+# or create a separate file for brew and one for apt.
+# (brew is not available on raspberry pi.)
 sudo apt update
 sudo apt install -y \
 	build-essential \
@@ -20,29 +21,37 @@ sudo apt install -y \
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
-# ripgrep:
+mkdir -p $HOME/.shelltools
+PLUGINSDIR=$HOME/.shelltools/zsh-plugins.sh
+
+
 brew install ripgrep
+brew install sqlite
+brew install yq
+
+
+brew install direnv
+echo 'eval "$(direnv hook zsh)"' >> $PLUGINSDIR
+
 
 brew install zoxide
 echo 'eval $(zoxide init zsh)' >> $PLUGINSDIR
 
+
 brew install fzf
 echo 'source <(fzf --zsh)' >> $PLUGINSDIR
 
-brew install sqlite
-
-
-# zsh plugins:
-mkdir -p $HOME/.shelltools
-PLUGINSDIR=$HOME/.shelltools/zsh-plugins.sh
 
 brew install zsh-history-substring-search
 echo 'source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh' >> $PLUGINSDIR
 
+
 brew install --quiet zsh-syntax-highlighting
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $PLUGINSDIR
 
+
 brew install --quiet zsh-autosuggestions
 echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $PLUGINSDIR
+
 
 git clone git@github.com:larkery/zsh-histdb.git $HOME/.shelltools/zsh-histdb

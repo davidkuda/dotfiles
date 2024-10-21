abb9bde587d8a4da546da4913f7a12216bea69b8
David Kuda <david.kuda@noser.com>
2024-10-21 15:43:41 +0200

    extract shade into its own file

---
+++ dot_config/nvim/lua/custom/plugins/shade.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'sunjon/shade.nvim',
+    opts = {
+      overlay_opacity = 70,
+      opacity_step = 1,
+      keys = {
+        brightness_up = '<Leader>hk',
+        brightness_down = '<Leader>hk',
+        toggle = '<Leader>ht',
+      },
+    },
+  },
+}
--- dot_config/nvim/lua/custom/plugins/varia.lua
+++ dot_config/nvim/lua/custom/plugins/varia.lua
@@ -54,16 +54,4 @@ return {
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
   },
-  {
-    'sunjon/shade.nvim',
-    opts = {
-      overlay_opacity = 70,
-      opacity_step = 1,
-      keys = {
-        brightness_up = '<Leader>hk',
-        brightness_down = '<Leader>hk',
-        toggle = '<Leader>ht',
-      },
-    },
-  },
 }

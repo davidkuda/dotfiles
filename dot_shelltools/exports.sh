dae8aa7e9e64908573faddaf29412d1344b76e29
David Kuda <david.kuda@noser.com>
2024-11-01 17:38:07 +0100

    update exports

--- dot_shelltools/exports.sh
+++ dot_shelltools/exports.sh
@@ -1,9 +1,11 @@
-export EDITOR=vim
 export PATH="$PATH:/opt/nvim-linux64/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/go/bin"
 export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/local/go/bin
 export PATH="$PATH:$HOME/.dotnet/tools"
+
+export EDITOR=vim
 export DELTA_PAGER='less -R'
 export C='/mnt/c/Users/david.kuda/Desktop/'
 

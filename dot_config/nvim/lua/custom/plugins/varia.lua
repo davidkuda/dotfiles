0b2f64b5aa06a483558f09d8d5251278decb40ae
David Kuda <davidkuda3@gmail.com>
2025-03-09 22:35:41 +0100

    use nvim-scrollbar

--- dot_config/nvim/lua/custom/plugins/varia.lua
+++ dot_config/nvim/lua/custom/plugins/varia.lua
@@ -1,8 +1,8 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
+  {
+    'petertriho/nvim-scrollbar',
+    opts = {},
+  },
 
   {
     'chentoast/marks.nvim',

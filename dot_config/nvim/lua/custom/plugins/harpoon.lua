85787465563db58cd6b22383b267ce6108566e7c
David Kuda <david.kuda@noser.com>
2025-03-18 14:00:22 +0100

    use numbers 1 to 0 for harpoon

--- dot_config/nvim/lua/custom/plugins/harpoon.lua
+++ dot_config/nvim/lua/custom/plugins/harpoon.lua
@@ -14,9 +14,12 @@ return {
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
       vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
       vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(4) end)
-      vim.keymap.set("n", "<leader>6", function() harpoon:list():select(4) end)
-      vim.keymap.set("n", "<leader>7", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+      vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+      vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+      vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+      vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
+      vim.keymap.set("n", "<leader>0", function() harpoon:list():select(10) end)
 
       -- Toggle previous & next buffers stored within Harpoon list
       -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)

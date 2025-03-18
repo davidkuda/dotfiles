636d114559ca73d5388b0ee20103b4f1b0c97e9f
David Kuda <david.kuda@noser.com>
2025-03-18 14:00:55 +0100

    no longer use - for oil

--- dot_config/nvim/lua/custom/plugins/oil.lua
+++ dot_config/nvim/lua/custom/plugins/oil.lua
@@ -24,7 +24,6 @@ return {
       }
       -- Open parent directory in current window
       vim.keymap.set('n', '<Leader>n', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
   },
 

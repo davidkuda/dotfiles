0fda5f5297ab29e9ede290350df22c4770e6a86f
David Kuda <davidkuda3@gmail.com>
2025-11-15 17:48:57 +0100

    use leader-l to open harpoon

--- dot_config/nvim/lua/custom/plugins/harpoon.lua
+++ dot_config/nvim/lua/custom/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
       harpoon:setup()
 
       vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<C-i>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>l", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
       vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
